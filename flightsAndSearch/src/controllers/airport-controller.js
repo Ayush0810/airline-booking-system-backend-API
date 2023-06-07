6696a575afd73f96c1067cb0a4fb19716293267d
ayush0810 <ayushksingh867@gmail.com>
2023-06-07 15:19:55 +0530

    added airport CRUD

---
+++ flightsAndSearch/src/controllers/airport-controller.js
@@ -0,0 +1,28 @@
+const {AirportService} = require("../service/index");
+
+const airportService = new AirportService()
+
+const create = async(req,res)=>{
+    try {
+        const  response = await airportService.create(req.body);
+        return res.status(201).json({
+            data:response,
+            success:true,
+            err:{},
+            message:"created a new airport"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            err:error,
+            message:"Cannot create a new airport"
+        })
+    }
+}
+
+module.exports={
+    create
+}
