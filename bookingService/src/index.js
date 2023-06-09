ce04e0c8e0d1d0ee846d383340e306a4331cde38
ayush0810 <ayushksingh867@gmail.com>
2023-06-09 16:02:45 +0530

    first commit with basic setup

---
+++ bookingService/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index")
+const setupAndStartServer = async () => {
+
+	const app = express();
+	//middlewares
+	app.use(express.json());
+	app.use(express.urlencoded({extended:true}));
+
+	app.use("/api" , apiRoutes);
+	
+	//setting up server
+	app.listen(PORT, () => {
+		console.log("Booking Server started at", PORT);
+		if(process.env.SYNC_DB){
+			db.sequelize.sync({alter: true});
+		}
+	});
+};
+
+setupAndStartServer();
