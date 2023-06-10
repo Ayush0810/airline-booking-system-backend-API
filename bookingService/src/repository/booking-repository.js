b718c1aad69b1b2b762d9ac1bbd0d4b6f9e829dd
ayush0810 <ayushksingh867@gmail.com>
2023-06-10 20:44:40 +0530

    added create booking service and fixed few bugs

--- bookingService/src/repository/booking-repository.js
+++ bookingService/src/repository/booking-repository.js
@@ -1,22 +1,41 @@
-const { Booking } = require("../models/index");
+const {booking}  = require("../models/index");
 const { ValidationError, AppError } = require("../utils/index");
 const { StatusCodes } = require("http-status-codes");
 
 class BookingRepository {
 	async create(data) {
-        try {
-        const booking = await Booking.create(data);
-        return booking;
-		
+		try {
+			const book = await booking.create(data);
+			return book;
 		} catch (error) {
 			if (error.name == "SequelizevalidationError") {
 				throw new ValidationError(error);
 			}
 
-			throw new AppError("RepositoryError", "Cannot Create booking", 
-            "there was some issue creating booking,please try again later",
-            StatusCodes.INTERNAL_SERVER_ERROR
-            );
+			throw new AppError(
+				"RepositoryError",
+				"Cannot Create booking",
+				"there was some issue creating booking,please try again later",
+				StatusCodes.INTERNAL_SERVER_ERROR
+			);
+		}
+	}
+
+	async update(bookingId, data) {
+		try {
+			const book = await booking.findByPk(bookingId);
+			if(data.status){
+				book.status = data.status
+			}
+			await book.save();
+			return book;
+		} catch (error) {
+			throw new AppError(
+				"RepositoryError",
+				"Cannot update booking",
+				"there was some issue updating booking,please try again later",
+				StatusCodes.INTERNAL_SERVER_ERROR
+			);
 		}
 	}
 }
