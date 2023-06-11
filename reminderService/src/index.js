f776295aceb95081140a9ff827d3a8ef21d605b4
ayush0810 <ayushksingh867@gmail.com>
2023-06-11 15:35:45 +0530

    added notification cron

--- reminderService/src/index.js
+++ reminderService/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const {PORT} = require("./config/serverConfig")
-
+const TicketController = require("./controllers/ticket-controller")
 const setUpAndStartServer = ()=>{
     const app = express();
     app.use(express.json())
     app.use(express.urlencoded({extended:true}));
+
+
+    app.post('/api/v1/tickets' , TicketController.create)
 
     app.listen(PORT,()=>{
         console.log("reminder service listening at port" ,PORT)
